0ceb6e4292edf691b5f0d40e26be0e5018da31f5
Cousi <javier.cousino.goicolea@hotmail.com>
2023-06-24 07:13:40 +0200

    Improvements

--- Subtitles/Index.js
+++ Subtitles/Index.js
@@ -1,15 +1,28 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-    
-    let video = document.querySelector("#video1");
-    let track = video.addTextTrack("captions", "Captions", "en");
-    
-    track.mode = "showing";
-    track.addCue(new VTTCue(0, 0.9, "Hildy!"));
-    track.addCue(new VTTCue(1, 1.4, "How are you?"));
-    track.addCue(new VTTCue(1.5, 2.9, "Tell me, is the lord of the universe in?"));
-    track.addCue(new VTTCue(3, 4.2, "Yes, he's in - in a bad humor"));
-    track.addCue(new VTTCue(4.3, 6, "Somebody must've stolen the crown jewels"));
-    
-    console.log(track.cues);
+    document.getElementById("AddSubtitle").addEventListener("click", (e) => {    
+        var subtitle = document.getElementById("SubtitleText").value;
+        var start = document.getElementById("Start").value;
+        var end = document.getElementById("End").value;
+        var video = document.getElementById("video1");
+        addSubTitle(subtitle, video, start, end);
+    });    
+
+    document.getElementById("AddSubtitleInmediate").addEventListener("click", (e) => {        
+        var subtitle = document.getElementById("SubtitleText").value;
+        var video = document.getElementById("video1");
+        addSubtitleInmediate(subtitle, video);
+    });
 
 });
+
+function addSubTitle(subtitle, video, startTime, endTime) {
+    let track = video.addTextTrack("captions", "Captions", "en");
+    track.mode = "showing";
+    track.addCue(new VTTCue(startTime, endTime, subtitle));
+}
+
+function addSubtitleInmediate(subtitle, video){
+    var currentTime = video.currentTime;
+    addSubTitle(subtitle, video, currentTime, currentTime + 1);
+}
